27100b7b16ac28298e8620cc99fd8e38c6c23d76
Om <autiom3123@gmail.com>
2025-10-08 17:01:30 +0530

    added required annotations

--- OfficeCelebr8/OfficeCelebr8.Application/DTOs/RegisterRequest.cs
+++ OfficeCelebr8/OfficeCelebr8.Application/DTOs/RegisterRequest.cs
@@ -1,11 +1,22 @@
-﻿namespace OfficeCelebr8.Application.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OfficeCelebr8.Application.DTOs
 {
     public class RegisterRequest
     {
+        [Required]
         public int EmployeeId { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required, EmailAddress]
         public string Email { get; set; }
+
+        [Required, MinLength(8), MaxLength(15)]
         public string Password { get; set; }
+
+        [Required]
         public string Designation { get; set; }
     }
 }
--- OfficeCelebr8/OfficeCelebr8.Domain/Models/User.cs
+++ OfficeCelebr8/OfficeCelebr8.Domain/Models/User.cs
@@ -1,15 +1,18 @@
-﻿using OfficeCelebr8.Domain.Constants;
+﻿using System.ComponentModel.DataAnnotations;
+using OfficeCelebr8.Domain.Constants;
 
 namespace OfficeCelebr8.Domain.Models
 {
     public class User
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
+        [Key]
         public int EmployeeId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string Designation { get; set; }
         public Role Role { get; set; } = Role.Employee;
+
     }
 }
